0273ab6d94f3fe7c4124bbf9783ccafbffebb6f6
STEP3DLab <projects.step3d@gmail.com>
2026-07-12 21:37:31 +0300

    Use clean authentic village background and local daisy assets

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.XB_APPS_SCRIPT_URL = '';
 window.XB_MUSIC_FILE = 'assets/ryazan-moya.mp3';
 window.XB_CONTACT_PHONE = '79969662393';
-document.documentElement.style.setProperty('--daisies-image', "url('https://pngimg.com/uploads/daisy/daisy_PNG17.png')");
+document.documentElement.style.setProperty('--daisies-image', "url('assets/daisy-cutout.webp')");
