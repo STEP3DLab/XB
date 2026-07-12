164cf99fe1739e4f6ef546ba8264897acede27a9
STEP3DLab <projects.step3d@gmail.com>
2026-07-12 17:20:51 +0300

    Use transparent photographic daisies in the final design

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 // После публикации Apps Script вставьте URL веб-приложения между кавычками.
 window.XB_APPS_SCRIPT_URL = '';
 window.XB_MUSIC_FILE = 'assets/ryazan-moya.mp3';
+// Фотографические ромашки с прозрачным фоном для декоративных краёв карточек.
+document.documentElement.style.setProperty('--daisies-image', "url('https://pngimg.com/uploads/daisy/daisy_PNG17.png')");
